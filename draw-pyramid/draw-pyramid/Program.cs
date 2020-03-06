7df2b803a3b4e5969dcaa67ebf51b73ff34b6185
Dan Hajek <danhajek2@seznam.cz>
2020-03-06 12:16:55 +0100

    multiplication-table

---
+++ draw-pyramid/draw-pyramid/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace draw_pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            Console.WriteLine("Enter the number of lines: ");
+            int numberOfLines = Convert.ToInt32(Console.ReadLine());
+
+            //constant values
+            int lengthOfTheLastLine = (numberOfLines -1) * 2 +1;  
+            int middleOfTheLine = (lengthOfTheLastLine / 2) + 1;
+            
+            // changing values
+            int indentation = middleOfTheLine -1 ;  // length of indentation = number of spaces
+            int charsOnCurrentLine = lengthOfTheLastLine - indentation * 2;  
+
+
+            for (int line = 1; line <= numberOfLines; line++)   // lines
+            {
+
+                for (int i = 0; i < indentation; i++)     //spaces on a line = indentation
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < charsOnCurrentLine; i++)  // asterisks on a line
+                {
+                    Console.Write("*");
+                }
+                
+                Console.WriteLine("");      // new line when you finish one line
+                
+                indentation -= 1;
+                charsOnCurrentLine += 2;
+            }
+        }
+    }
+}
--- week-01/day-05/multiplication-tble/multiplication-tble/Program.cs
+++ week-01/day-05/multiplication-tble/multiplication-tble/Program.cs
@@ -6,7 +6,33 @@ namespace multiplication_tble
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a program that asks for a number and prints the multiplication table with that number
+            //
+            // Example:
+            // The number 15 should print:
+            //
+            // 1 * 15 = 15
+            // 2 * 15 = 30
+            // 3 * 15 = 45
+            // 4 * 15 = 60
+            // 5 * 15 = 75
+            // 6 * 15 = 90
+            // 7 * 15 = 105
+            // 8 * 15 = 120
+            // 9 * 15 = 135
+            // 10 * 15 = 150
+
+
+            Console.WriteLine("Please enter a number: ");
+            bool isNumber = Int32.TryParse(Console.ReadLine(), out int num);
+
+            if (num > 0)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{i} * {num} = {i * num}");
+                }
+            }
         }
     }
 }
