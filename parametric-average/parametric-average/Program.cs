3760415e86e56dbd400ced9b6f70c3bf64911912
Dan Hajek <danhajek2@seznam.cz>
2020-03-06 17:26:21 +0100

    parametric-average

---
+++ parametric-average/parametric-average/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace parametric_average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            bool isNumber = false;
+            int howManyNumbers = 0;
+            int enteredNumber;
+            double sum = 0;
+
+            while (!isNumber) {
+                Console.WriteLine("Enter how many numbers you want to enter: ");
+                isNumber = int.TryParse(Console.ReadLine(), out howManyNumbers);
+                if (isNumber)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("whole numbers only");
+                }
+            }
+            for(int i = 0; i < howManyNumbers; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Enter the {i + 1}. number: ");
+                    isNumber = int.TryParse(Console.ReadLine(), out enteredNumber);
+                    if (isNumber)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter only numbers.");
+                    }
+                }
+
+                sum += enteredNumber;
+            }
+            double average = sum / howManyNumbers;
+            Console.WriteLine($"sum: {sum}");
+            Console.WriteLine($"average: {average}");
+
+
+
+
+        }
+    }
+}
