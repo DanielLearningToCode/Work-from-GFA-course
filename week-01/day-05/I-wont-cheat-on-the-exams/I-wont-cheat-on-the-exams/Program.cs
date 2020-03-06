610bb368f660bc1c6b18181f3c6369dafa707ffd
Dan Hajek <danhajek2@seznam.cz>
2020-03-06 11:56:49 +0100

    I-Wont-Cheat-On-The-Exams

---
+++ week-01/day-05/I-wont-cheat-on-the-exams/I-wont-cheat-on-the-exams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace I_wont_cheat_on_the_exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i + ". I won't cheat on the exam!");
+            }
+        }
+    }
+}
