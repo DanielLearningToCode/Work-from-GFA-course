93a9e433136cfd6dee91559f6d4609926a7d918a
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 12:38:19 +0100

    third

---
+++ week-02/day-01/nested-for-loops-Misi/nested-for-loops-Misi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace nested_for_loops_Misi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    {
+                        Console.Write(
+"xxxxxxxxxx"
+"xx      xx"
+"x x    x x"
+"x  x  x  x"
+"x   xx   x"
+"x   xx   x"
+"x  x  x  x"
+"x x    x x"
+"xx      xx"
+"xxxxxxxxxx");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ week-02/day-01/nested-for-loops/nested-for-loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace nested_for_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines;
+            Console.WriteLine("Enter the number of lines:");
+            bool isNumber = int.TryParse(Console.ReadLine(), out numberOfLines);
+
+            
+            int numberOfColumns = numberOfLines;
+        
+
+            for (int line = 0; line < numberOfLines; line++)
+            {
+                for (int column = 0; column < numberOfColumns; column++)
+                {
+                    if(line == 0 || line == numberOfLines -1 || column == 0 || 
+                                column == numberOfColumns -1 || line == column
+                                 || line + column == numberOfLines -1)
+                    Console.Write("#");
+                    else
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
