cc7149aedb4a23315c29138f01d2966a5f300e8d
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 13:06:28 +0100

    Matrix

---
+++ week-02/day-01/DiagonalMatrix/DiagonalMatrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; column++)
+                {
+                    if (row != column)
+                    {
+                        Console.Write("0");
+                    }
+                    else
+                    {
+                        Console.Write("1");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
