09ec395a42d3a01ae17a566936e03eaa0af1df8d
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 16:09:50 +0100

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `Factorio`
+            //   that returns it's input's factorial
+            int input = UserInput();
+            int result = Factorial(input);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        public static int Factorial(int number)
+        {
+            int num = 1;
+            for (int i = number; i > 0; i--)
+            {
+                num *= i;
+            }
+            return num;
+
+        }
+
+        public static int UserInput()
+        {
+            Console.WriteLine("Enter the range");
+            int.TryParse(Console.ReadLine(), out int input);
+            return input;
+        }
+    }
+}
