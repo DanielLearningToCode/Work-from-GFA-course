0da9b72395af5e5395294a0c56ff867eb4f3ae10
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 15:30:02 +0100

    Summing

---
+++ week-02/day-01/Summing/Summing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Summing
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+            int range = UserInput();
+            int sum = Sum(range);
+            Console.WriteLine($"The sum of {range} is: {sum}");
+
+        }
+
+
+        public static int Sum (int range)
+        {
+            int sum = 0;
+            for (int i = 0; i <= range; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public static int UserInput()
+        {
+            Console.WriteLine("Enter the range");
+            int.TryParse(Console.ReadLine(), out int input);
+            return input;
+        }
+
+    }
+}
