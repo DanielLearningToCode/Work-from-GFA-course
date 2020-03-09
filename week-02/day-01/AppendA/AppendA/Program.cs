c727f70d340df26fb0851753c96c6f86fdd339e2
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 13:31:23 +0100

    AppendA

---
+++ week-02/day-01/AppendA/AppendA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `animals`
+            //   with the following content: `["koal", "pand", "zebr"]`
+            // - Add all elements an `"a"` at the end
+            string[] animals = { "koal", "pand", "zebr" };
+
+            for(int i = 0; i < animals.Length; i++)
+            {
+                animals[i] += "a";
+            }
+            
+            for (int j = 0; j < animals.Length; j++)
+            {
+                Console.WriteLine(animals[j]);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
