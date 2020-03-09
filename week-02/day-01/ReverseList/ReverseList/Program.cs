51ac2ea6818ee2b087222988c217a146bf7ab200
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 13:55:15 +0100

    ReverseList

---
+++ week-02/day-01/ReverseList/ReverseList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ReverseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+
+            int[] aj = { 3, 4, 5, 6, 7 };
+
+            //Array.Reverse(aj);
+            //Array.Sort(aj);
+            /*for(int i = 0; i < aj.Length; i++)
+            {
+                Console.WriteLine(aj[i]);
+            }*/
+            
+
+            for (int i = 0; i < aj.Length / 2; i++)
+            {
+                int temp = aj[i];
+                aj[i] = aj[aj.Length - 1 - i];
+                aj[aj.Length - 1 - i] = temp;
+            }
+
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.WriteLine(aj[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
