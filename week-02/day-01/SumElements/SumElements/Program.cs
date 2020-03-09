81fbbd4ee0968066d02be740fdeb19913dd26e23
Dan Hajek <danhajek2@seznam.cz>
2020-03-09 12:43:34 +0100

    CompareLength

---
+++ week-02/day-01/SumElements/SumElements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
