f0565d455c447f0378e184081ed97fbae786c057
Dan Hajek <danhajek2@seznam.cz>
2020-03-10 17:25:39 +0100

    ShoppingList

---
+++ week-02/day-02/ShoppingList/ShoppingList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shoppingList = new List<string>() { "Eggs", "milk", "fish", "apples", "bread", "chicken" };
+            IsInList("bananas ", shoppingList);
+        }
+        public static void IsInList (string searchedItem, List<string> shoppingList)
+        {
+            int counter = 0;
+            foreach (var item in shoppingList)
+            {
+                if (item.ToLower().Trim() == searchedItem.ToLower().Trim())
+                {
+                    Console.WriteLine("The {0} is in the list",searchedItem);
+                    counter = 0;
+                    break;
+                }
+                counter++;
+            }
+
+            if (counter != 0)
+            {
+                Console.WriteLine("The {0} is not in the list", searchedItem);
+            }
+        }
+    }
+}
