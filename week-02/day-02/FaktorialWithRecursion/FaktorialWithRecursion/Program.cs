b37a5e9075695ce17c86a574e4cc01f4c508beb8
Dan Hajek <danhajek2@seznam.cz>
2020-03-10 10:50:04 +0100

    SimpleReplace

---
+++ week-02/day-02/FaktorialWithRecursion/FaktorialWithRecursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FaktorialWithRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Faktorial(6));
+        }
+
+        public static int Faktorial (int num)
+        {
+
+            if(num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Faktorial(num - 1);
+            }
+
+            
+                
+        }
+    }
+
+}
