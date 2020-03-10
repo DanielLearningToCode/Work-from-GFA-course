c31cff852b479524269a91e8c5bf313d1109b1ce
Dan Hajek <danhajek2@seznam.cz>
2020-03-10 11:16:53 +0100

    UrlFixer

--- week-02/day-02/URLFixer/URLFixer/Program.cs
+++ week-02/day-02/URLFixer/URLFixer/Program.cs
@@ -6,7 +6,33 @@ namespace URLFixer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crucial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            // 1st method
+            /*url = url.Replace("bots", "odds");
+            url = url.Insert(5, ":");
+*/
+            // 2nd method
+            string[] array = url.Split("/");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "https")
+                {
+                    array[i] = "https:";
+                }
+                if (array[i] == "nevertellmethebots")
+                {
+                    array[i] = "nevertellmetheodds";
+                }
+            }
+
+            url = String.Join("/", array);
+
+            Console.WriteLine(url);
         }
     }
 }
