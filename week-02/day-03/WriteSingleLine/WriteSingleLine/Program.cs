eb33453f6c5765035aca724b9cd8339aef88572c
Dan Hajek <danhajek2@seznam.cz>
2020-03-11 16:26:03 +0100

    Write-multiple-lines

---
+++ week-02/day-03/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that is able to manipulate a file
+            // By writing your name into it as a single line
+            // The file should be named "my-file.txt"
+            // In case the program is unable to write the file,
+            // It should print the following error message: "Unable to write file: my-file.txt"
+
+            WriteLine("Dan");
+        }
+        public static void WriteLine(string text)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter("my-file.txt", append: true) ;
+                writer.WriteLine(text);
+                writer.Dispose();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to write file: my - file.txt");
+            }
+        }
+
+    }
+}
---
+++ week-02/day-03/write-multiple-lines/write-multiple-lines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+namespace write_multiple_lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes 3 parameters: a path, a word and a number
+            // and is able to write into a file.
+            // The path parameter should be a string that describes the location of the file you wish to modify
+            // The word parameter should also be a string that will be written to the file as individual lines
+            // The number parameter should describe how many lines the file should have.
+            // If the word is 'apple' and the number is 5, it should write 5 lines
+            // into the file and each line should read 'apple'
+            // The function should not raise any errors if it could not write the file.
+            WriteLines(Console.ReadLine(), "apple", 5); 
+
+        }
+        public static void WriteLines (string path, string word, int numberOfLines)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(path);
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    writer.WriteLine(word);
+                }
+                writer.Dispose();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("an error occured");
+            }
+        }
+
+    }
+}
