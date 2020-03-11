b4e7ec9be7b175a7f0220db6fbe33c5705d6ac87
Dan Hajek <danhajek2@seznam.cz>
2020-03-11 14:24:50 +0100

    CountLines

---
+++ week-02/day-03/CountLines/CountLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+
+            Console.WriteLine(NumberOfLines()); ;
+        }
+        public static int NumberOfLines()
+        {
+            Console.WriteLine("Enter the file name");
+            try
+            {
+                string fileName = Console.ReadLine();
+                int lineCount = File.ReadAllLines(fileName).Length;
+                return lineCount;
+            }
+            catch (FileNotFoundException)
+            {
+                return 0;
+                
+            }
+            catch (IOException)
+            {
+                return 0;
+            }
+            
+        }
+
+    }
+}
