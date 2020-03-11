cc066582bcd093dd69eb45772838fa3e7f148df5
Dan Hajek <danhajek2@seznam.cz>
2020-03-11 13:25:56 +0100

    DivideByZero

---
+++ week-02/day-02/ProductDatabase/ProductDatabase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var database = new Dictionary<string, int>()
+            {
+                {"Milk",200},
+                {"Eggs",200},
+                {"Fish",400},
+                {"Apples",150},
+                {"Bread",50},
+                {"Chicken",550}
+            };
+        }
+    }
+}
---
+++ week-02/day-03/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // It should print "fail" if the parameter is 0
+
+            DivideTen();
+        }
+
+        public static void DivideTen()
+        {
+            bool isNumber = false; 
+            int number = 0;
+            do
+            {
+                Console.WriteLine("Please enter a whole number as a divisor: ");
+                isNumber = Int32.TryParse(Console.ReadLine(), out number);
+
+            } while (!isNumber);
+
+            try
+            {
+                Console.WriteLine(10 / number);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
+
