a20f64d3b3d3612339fc6f64a2fb51bbba8efebf
Dan Hajek <danhajek2@seznam.cz>
2020-03-12 15:57:42 +0100

    Diagonals

---
+++ week-02/day-04/Diagonals/Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace Diagonals
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            var height = Height;
+            var width = Width;
+            ////
+            // Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+            var upperLeft = new Point(0, 0);
+            var upperRight = new Point(width, 0);
+            var bottomLeft = new Point(0, height);
+            var bottomRight = new Point(width, height);
+
+            foxDraw.SetStrokeColor(Colors.Green);
+            foxDraw.DrawLine(upperLeft, bottomRight);
+            foxDraw.SetStrokeColor(Colors.Red);
+            foxDraw.DrawLine(bottomLeft, upperRight);
+
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
