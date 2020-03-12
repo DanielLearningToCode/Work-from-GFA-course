3f22e3936e3724ac07e2e4f2ccb46b62c9e574b7
Dan Hajek <danhajek2@seznam.cz>
2020-03-12 16:22:24 +0100

    HorizontalLines

---
+++ week-02/day-04/HorizontalLines/HorizontalLines/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+using System;
+
+namespace HorizontalLines
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            ////
+            // Create a function that draws a single line and takes 3 parameters:
+            // The x and y coordinates of the line's starting point and the foxDraw
+            // and draws a 50 long horizontal line from that point.
+            // Draw at least 3 lines with that function using a loop.
+
+            var offset = 5;
+            for (int i = 0; i < 50; i++)
+            {
+            Lines(100, 100 + offset * i, foxDraw);
+            }
+#endif
+        }
+
+        private void Lines(double x, double y, FoxDraw foxDraw)
+        {
+            foxDraw.DrawLine(x, y, x + 50, y);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
