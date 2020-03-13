4cf4f07c3f46fcf5936d3ebe5176c663a90b4a81
Dan Hajek <danhajek2@seznam.cz>
2020-03-13 12:28:20 +0100

    LinePlay

---
+++ week-02/day-05/LinePlay/LinePlay/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace LinePlay
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            ////
+
+            var shift = Width / 15;
+            foxDraw.SetStrokeColor(Colors.Orchid);
+            for (int i = 0; i < 15; i++)
+            {
+            foxDraw.DrawLine(shift * i, 0, Width, shift * i);
+            }
+            foxDraw.SetStrokeColor(Colors.Lime);
+            for (int i = 0; i < 15; i++)
+            {
+                foxDraw.DrawLine(0, shift *i, shift * i, Height);
+            }
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
