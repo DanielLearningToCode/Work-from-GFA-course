259fbf8d74a3af7048edfbdd2dc9ce5ceb3f6786
Dan Hajek <danhajek2@seznam.cz>
2020-03-16 13:22:50 +0100

    Power

---
+++ week-03/day-01/Power/Power/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Given base and n that are both 1 or more, compute recursively (no loops) 
+            ///the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+            ///
+            Console.WriteLine("Enter the number you raise to a power");
+            int baseNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the power: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PowerRecursively(baseNum, exponent));
+        }
+
+        private static int PowerRecursively(int baseNum, int exponent)
+        {
+            if (exponent == 1)
+            {
+                return baseNum;
+            }
+            return baseNum * PowerRecursively(baseNum, exponent - 1);
+        }
+    }
+}
