ae9f66c57e4a66a4ad40c78af4444bb22a32b75b
Dan Hajek <danhajek2@seznam.cz>
2020-03-16 17:08:15 +0100

    Bunnies

---
+++ week-03/day-01/Bunnies/Bunnies/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively
+            // (without loops or multiplication).
+            Console.WriteLine("Enter the number of bunnies: ");
+            int numberOfBunnies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountEars(numberOfBunnies)); 
+        }
+
+        private static int CountEars(int numberOfBunnies)
+        {
+            if (numberOfBunnies < 1)
+            {
+                throw new ArgumentOutOfRangeException ("Enter positive integers only");
+            }
+            else
+            {
+                if (numberOfBunnies == 1)
+                {
+                    return 2;
+                }
+                return 2 + CountEars(numberOfBunnies - 1);
+            }
+        }
+
+    }
+}
--- week-03/day-01/sum-digits/sum-digits/Program.cs
+++ week-03/day-01/sum-digits/sum-digits/Program.cs
@@ -12,10 +12,19 @@ namespace sum_digits
             Console.WriteLine(SumDigits(givenNumber));
             Console.ReadLine();
         }
-        // alternative using modulo operator
-        public static int SumDigits(int n)
-        {
 
+        public static int SumDigits(int number)
+        {
+            if (number < 10)
+            {
+                return number;
+            }
+            return number % 10 + SumDigits(number / 10);
+        }
+
+        // an unnecessaryly complicated alternative using modulo operator
+       /* public static int SumDigits(int n)
+        {
             double numOfDigits = n.ToString().Length;
             if (numOfDigits == 1)
             {
@@ -23,7 +32,7 @@ namespace sum_digits
             }
             return n / Convert.ToInt32(Math.Pow(10, numOfDigits - 1))
                 + SumDigits(n % Convert.ToInt32(Math.Pow(10, numOfDigits - 1)));
-        }
+        }*/
         // alternative using string manipulation
         /*        public static int SumDigits(string givenNumber)
                 {
