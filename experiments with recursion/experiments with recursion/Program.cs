b1d7832b47fb5732ccb7d7eb14efe354d498bd75
Dan Hajek <danhajek2@seznam.cz>
2020-03-17 21:52:47 +0100

    Triangles

---
+++ experiments with recursion/experiments with recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace experiments_with_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = "text";
+            Console.WriteLine(str.Substring(2, str.Length- 2)); 
+            //Console.WriteLine(Strings("This text contains lot of xxx characters"));
+        }
+
+        public static string Strings(string str)
+        {
+            //Given a string, compute recursively(no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+            if (str.Length == 0) return "";
+            if (str[0].Equals('x')) return "y" + Strings(str.Substring(1, str.Length - 1));
+            else return str[0] + Strings(str.Substring(1, str.Length - 1));
+        }
+    }
+}
---
+++ week-03/day-02/Triangles/Triangles/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+using System;
+using System.Collections.Generic;
+
+namespace Triangles
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            ////
+            ///
+            foxDraw.SetFillColor(RandomColor());
+            DrawTriangle(0, 0, Width, 10, foxDraw);
+#endif
+        }
+
+        public static Color RandomColor()
+        {
+            Random rnd = new Random();
+            List<Color> colors = new List<Color>
+            {
+                Colors.AliceBlue,
+                Colors.Aquamarine,
+                Colors.Azure,
+                Colors.BlueViolet,
+                Colors.Chocolate,
+                Colors.Cyan,
+                Colors.DarkGoldenrod,
+                Colors.DarkMagenta,
+                Colors.DarkRed,
+                Colors.Yellow,
+                Colors.Gold,
+                Colors.Purple,
+                Colors.Red,
+                Colors.Pink,
+                Colors.SeaShell,
+                Colors.Silver,
+                Colors.Green
+            };
+            Color randColor = colors[rnd.Next(0, colors.Count)];
+            return randColor;
+        }
+        private void DrawTriangle(double x, double y, double size, int level, FoxDraw foxDraw)
+        {
+            if (level == 0)
+            {
+                return;
+            }
+            // upside down triangle
+            double halfOfSide = size / 2;
+            //double twoThirds = 2 * halfOfSide;
+            var leftTop = new Point(x, y);
+            var rightTop = new Point(x + size, y);
+            var heightReversedTriangle = Math.Sqrt((size * size) - (Math.Pow(0.5 * size, 2)));
+            var bottom = new Point(x + halfOfSide, y + heightReversedTriangle);
+            var bottomUpTriangle = new List<Point>() { leftTop, rightTop, bottom };
+            foxDraw.DrawPolygon(bottomUpTriangle);
+            // right way up triangle
+            var heightUpTriangle = Math.Sqrt((halfOfSide * halfOfSide) - (Math.Pow(0.5 * halfOfSide, 2)));
+            var bottomLeft = new Point(x + 0.5 * halfOfSide, y + heightUpTriangle);
+            var bottomRight = new Point(x + 0.5 * halfOfSide + halfOfSide, y + heightUpTriangle);
+            var top = new Point(x + halfOfSide, y);
+            var topUpTriangle = new List<Point>() { bottomLeft, bottomRight, top };
+            foxDraw.DrawPolygon(topUpTriangle);
+
+            level--;
+            foxDraw.SetFillColor(RandomColor());
+            DrawTriangle(x, y, size * 0.5, level, foxDraw);
+            foxDraw.SetFillColor(RandomColor());
+            DrawTriangle(x + halfOfSide, y, size * 0.5, level, foxDraw);
+            foxDraw.SetFillColor(RandomColor());
+            DrawTriangle(x + halfOfSide * 0.5, y + heightUpTriangle, size * 0.5 , level, foxDraw);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
