2e22189c57afce64cba3fa07340b703664d75ab4
Dan Hajek <danhajek2@seznam.cz>
2020-03-18 15:58:17 +0100

    Sharpie

---
+++ week-03/day-03/sharpie/sharpie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie bluePen = new Sharpie("Blue", 1);
+            while (bluePen.InkAmount > 0)
+            {
+                bluePen.Use();
+                Console.WriteLine(bluePen.InkAmount);
+            }
+        }
+    }
+}
---
+++ week-03/day-03/sharpie/sharpie/Sharpie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpie
+{
+    class Sharpie
+    {
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float InkAmount { get; set; }
+
+        public Sharpie(string color, float width)
+        {
+            this.InkAmount = 100;
+        }
+        public void Use()
+        {
+            this.InkAmount--;
+        }
+    }
+}
+
+/*Create Sharpie class
+    We should know about each sharpie their Color(which should be a string),
+    Width(which will be a floating point number), InkAmount(another floating point number)
+    When creating one, we need to specify the Color and the Width
+    Every sharpie is created with a default 100 as InkAmount
+    We can Use() the sharpie objects
+        which decreases inkAmount
+*/
