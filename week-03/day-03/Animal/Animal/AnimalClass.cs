7d13d3c687b11db108f59b33e6dac8b296efada4
Dan Hajek <danhajek2@seznam.cz>
2020-03-18 15:48:40 +0100

    Animal

---
+++ week-03/day-03/Animal/Animal/AnimalClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal
+{
+    class AnimalClass
+    {
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+
+        public AnimalClass()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+        public void Eat()
+        {
+            Hunger--;
+        }
+        public void Drink()
+        {
+            Thirst--;
+        }
+        public void Play()
+        {
+            if (Hunger < 100 && Thirst < 100)
+            {
+                Hunger++;
+                Thirst++;
+            }
+            else
+            {
+                Hunger++;
+                Thirst++;
+                Console.WriteLine("You are about to be eaten"); 
+            }
+        }
+    }
+}
+
+   /* Every animal has a hunger value, which is a whole number
+    Every animal has a thirst value, which is a whole number
+    when creating a new animal object these values are created with the default 50 value
+    Every animal can Eat() which decreases their hunger by one
+    Every animal can Drink() which decreases their thirst by one
+    Every animal can Play() which increases both by one
+*/
---
+++ week-03/day-03/Animal/Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalClass dog = new AnimalClass();
+            Console.WriteLine(dog.Hunger);
+            while (dog.Hunger <= 100)
+            {
+                dog.Play();
+                Console.WriteLine(dog.Hunger);
+            }
+            Console.WriteLine();
+        }
+    }
+}
