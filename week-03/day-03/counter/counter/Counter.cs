9de8e61135802bbb2bec85c704e7f53e3a0c09de
Dan Hajek <danhajek2@seznam.cz>
2020-03-18 16:11:08 +0100

    Counter

---
+++ week-03/day-03/counter/counter/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace counter
+{
+    class Counter
+    {
+        public int Count { get; set; }
+
+        public Counter()
+        {
+            this.Count = 0;
+        }
+        public Counter(int initialValue)
+        {
+            this.Count = initialValue;
+        }
+        public void Add()
+        {
+            this.Count++;
+        }
+        public void Add(int number)
+        {
+            this.Count += number;
+        }
+        public string Get()
+        {
+            return this.Count.ToString();
+        }
+        public void Reset()
+        {
+            this.Count = 0;
+        }
+    }
+}
+
+/*    which has an integer property
+    when creating it should have a default value 0 or we can specify it when creating
+    we can Add(number) to this counter another whole number
+    or we can Add() without parameters just increasing the counter's value by one
+    and we can Get() the current value as string
+    also we can Reset() the value to the initial value
+*/
---
+++ week-03/day-03/counter/counter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter myCounter = new Counter();
+            myCounter.Add();
+            Console.WriteLine(myCounter.Get());
+            myCounter.Add(5);
+            Console.WriteLine(myCounter.Count);
+            Console.WriteLine(myCounter.Get()) ;
+            myCounter.Reset();
+            Console.WriteLine(myCounter.Get());
+
+            Counter secondCounter = new Counter(10);
+            Console.WriteLine(secondCounter.Get()); 
+
+        }
+    }
+}
