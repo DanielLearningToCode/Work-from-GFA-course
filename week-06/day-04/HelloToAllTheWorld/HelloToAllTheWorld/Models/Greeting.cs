7247f36539916dad9955a035c07a494241f5e464
Dan Hajek <danhajek2@seznam.cz>
2020-04-16 17:55:00 +0200

    web-app

---
+++ week-06/day-04/HelloToAllTheWorld/HelloToAllTheWorld/Models/Greeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloToAllTheWorld.Models
+{
+    
+    public class Greeting
+    {
+        public string Name { get; set; }
+        public List<string> HelloList { get; set; }
+        string[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+                "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+                "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+                "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+        public Greeting()
+        {
+            HelloList = hellos.ToList();
+        }
+        
+    }
+}
