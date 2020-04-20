7a2d91265f304a39243fa9628c119997ffd2017a
Dan Hajek <danhajek2@seznam.cz>
2020-04-20 14:35:43 +0200

    Bank of Simba, first part

---
+++ week-07/day-01/Bank-of-Simba/Bank-of-Simba/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bank_of_Simba.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace Bank_of_Simba.Controllers
+{   //[Route("Home")]
+    public class HomeController : Controller
+    {
+        public static List<BankAccount> accountList = null;
+        public static void InitializeList()
+        {
+            accountList = new List<BankAccount>()
+            {
+                new BankAccount("Zazu", 500, "bird", false),
+                new BankAccount("Scar", 10000, "lion", false),
+                new BankAccount("Timon", 800, "meerkat", false),
+                new BankAccount("Rafiky", 5000, "baboon", false),
+                new BankAccount("Simba", 2000, "lion", true),
+            };
+        }
+        public HomeController()
+        {
+            if (accountList ==null)
+            {
+                InitializeList();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View("index" );
+        }
+
+        public IActionResult Html()
+        {
+            string message = "This is an<em> HTML </ em > text. < b > Enjoy yourself! </ b > ";
+            ViewBag.Message = message;
+            return View("index");
+        }
+        public IActionResult ListAllAccounts()
+        {
+            return View(accountList);
+        }
+    }
+}
+
+
+///In your controller create the first endpoint called /show
+/*Instantiate a new BankAccount("Simba", 2000, "lion")
+Java: Add this BankAccount to the model
+C#: Add this BankAccount to the View
+Create a template where you show the fields of the BankAccount*/
+/*public static List<BankAccount> InitializeList()
+{
+    List<BankAccount> accountList = new List<BankAccount>();
+        public BankAccount simba = new BankAccount("Simba", 2000, "lion");
+public BankAccount zazu = new BankAccount("Zazu", 500, "bird");
+public BankAccount scar = new BankAccount("Scar", 10000, "lion");
+public BankAccount timon = new BankAccount("Timon", 800, "meerkat");
+public BankAccount rafiky = new BankAccount("Rafiky", 5000, "baboon");
+            return 
+        }*/
---
+++ week-07/day-01/Bank-of-Simba/Bank-of-Simba/Models/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank_of_Simba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; private set; }
+        public decimal Balance { get; private set; }
+        public string AnimalType { get; private set; }
+        public bool King { get; private set; }
+
+
+        public BankAccount(string name, decimal balance, string animalType, bool king)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            King = king;
+        }
+
+        public BankAccount()
+        {
+        }
+    }
+}
