a64b17fa303d7c48169047527ac5c447a6f8a164
Dan Hajek <danhajek2@seznam.cz>
2020-04-27 13:24:44 +0200

    Todo app from the list

---
+++ week-08/EntityFrameworkBasics/EntityFrameworkBasics/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkBasics.Controllers
+{
+    [Route("todo")]
+    public class TodoController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("")]
+        [Route("list")]
+        public IActionResult List()
+        {
+            List<ToDo> todos = new List<ToDo>() {
+                new ToDo { Id = 1, Title = "Sleep", IsDone = false, IsUrgent = true },
+                new ToDo{Id = 2, Title = "Work", IsDone = false, IsUrgent = false},
+                new ToDo{Id = 3, Title = "Buy milk", IsDone = true, IsUrgent = false } };
+            return View(todos);
+        }
+    }
+}
---
+++ week-08/EntityFrameworkBasics/EntityFrameworkBasics/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkBasics.Models
+{
+    public class ToDo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
+/////Add a Todo class with the following properties:
+///(Id: long, Title: string, IsUrgent: boolean(default false), IsDone: boolean(default false))
