fc04b99325e05c9334ae6e020515d9afc956ddd1
Dan Hajek <danhajek2@seznam.cz>
2020-05-04 12:11:35 +0200

    REST

---
+++ week-09/REST/REST/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using REST.Models;
+
+namespace REST.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet("doubling")]
+        public Doubling Doubling(int? input)
+        {
+            Doubling result;
+            if (input == null)
+            {
+                result = new Doubling() { Error = "Please provide an input!" };
+                return result;
+            }
+            result = new Doubling(input);
+            return result;
+        }
+    }
+}
---
+++ week-09/REST/REST/Models/Doubling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.Models
+{
+    public class Doubling
+    {
+        public int? Received { get; set; }
+        public int Result { get; set; }
+        public string Error { get; set; }
+        public Doubling()
+        {
+
+        }
+        public Doubling(int? input)
+        {
+            Received = input;
+            Result = (int)input * 2;
+        }
+
+    }
+}
