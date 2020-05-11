bb368f31808462909d9197b8e1d5da4841b7afe3
Dan Hajek <danhajek2@seznam.cz>
2020-05-11 19:19:04 +0200

    chat app basic functionality

---
+++ week-10/ChatApp/ChatApp/Models/MessageToSend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class MessageToSend
+    {
+        public int? ChannelId { get; set; }
+        public string ChannelSecret { get; set; }
+        public string Content { get; set; }
+    }
+}
